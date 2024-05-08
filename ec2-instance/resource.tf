2a0490e279174c515cc74fa9114b1e62b3269723
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-05-08 14:47:01 +0530

    Update resource.tf

--- ec2-instance/resource.tf
+++ ec2-instance/resource.tf
@@ -2,6 +2,8 @@ resource "aws_instance" "eoc-instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id = var.subnet_id
+  tags = [
+      Name = "MYEC2-VM"
 }
 resource "aws_vpc" "main" {
   cidr_block = "172.30.0.0/16"
