c46d5887236c398161aa7df1f6c2e55b7e095a02
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-05-08 15:07:15 +0530

    Update resource.tf

--- ec2-instance/resource.tf
+++ ec2-instance/resource.tf
@@ -1,11 +1,53 @@
 resource "aws_instance" "eoc-instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id = var.subnet_id
   tags = [
       Name = "MYEC2-VM"
 }
-resource "aws_vpc" "main" {
-  cidr_block = "172.30.0.0/16"
+
+#create a VPC
+resource "aws_vpc" "main-vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+tags {
+    Name = "prod-vpc"
+  }
+}
+
+#create a public Subnet
+resource “aws_subnet” “dev-subnet-public-1” {
+  vpc_id = “${aws_vpc.main-vpc.id}”
+  availability_zone = var.aws_region
+
+tags {
+     Name = "dev-subnet-public-1"
+  }
+}
+
+#create a Security Group
+resource "aws_security_group" "ssh-allowed" {
+    vpc_id = "${aws_vpc.main-vpc.id}"
+    
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+} 
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags {
+        Name = "ssh-allowed"
+    }
 }
 
