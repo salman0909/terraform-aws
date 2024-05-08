36d3f32a8c20275663ce89a6daf89c8b83891766
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-05-08 15:43:31 +0530

    Update resource.tf

--- ec2-instance/resource.tf
+++ ec2-instance/resource.tf
@@ -2,19 +2,20 @@ resource "aws_instance" "eoc-instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
   associate_public_ip_address = true
+  key_name = aws_key_pair.key_pair.key_name
   tags = [
-      Name = "MYEC2-VM"
+      Name = "eoc-instance"
 }
 
 #create a Pem file formatted private key
-resource "tls_private_key" "rsa-4096-example" {
+resource "tls_private_key" "rsa-4096" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 # create a aws key pair resource
-resource "aws_key_pair" "key_pait" {
-  key_name   = "deployer-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+resource "aws_key_pair" "key_pair" {
+  key_name   = "my-key"
+  public_key = "tls_private_key.rsa-4096.public_key_openssh"
 }
 #create a VPC
 resource "aws_vpc" "main-vpc" {
