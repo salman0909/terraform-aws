8dcb194a5bc82f0adc14bce51d0ee58334f8c000
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-05-08 13:10:43 +0530

    Update resource.tf

--- ec2-instance/resource.tf
+++ ec2-instance/resource.tf
@@ -1,6 +1,23 @@
 resource "aws_instance" "eoc-instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  vpc_id = var.vpc_id
-  subnet_id = var.subnet_id
+ #vpc_id = var.vpc_id
+#subnet_ids = var.subnet_id
+}
+
+resource "aws_vpc" "example" {
+  cidr_block = "172.30.0.0/16"
+  tags = {
+    Name = "tf-example"
+  }
+}
+
+resource "aws_subnet" "example" {
+  vpc_id            = var.vpc_id
+  cidr_block        = "172.30.0.0/16"
+  subnet_ids = var.subnet_id
+
+  tags = {
+    Name = "tf-example"
+  }
 }
