eb82ee0aa07978a0dcd0f074749629b5230de85f
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-05-09 17:58:29 +0530

    Update resource.tf

--- count_argument/resource.tf
+++ count_argument/resource.tf
@@ -1,3 +1,20 @@
+#create a VPC
+resource "aws_vpc" "main-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main-vpc"
+  }
+}
+#create a public Subnet
+resource "aws_subnet" "dev-subnet-public-1" {
+  vpc_id = "${aws_vpc.main-vpc.id}"
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-2a"
+  tags = {
+     Name = "dev-subnet-public-1"
+  }
+}
 resource "aws_instance" "eoc-instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
