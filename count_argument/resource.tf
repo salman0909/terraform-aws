b913e2c9051d853e4322bb1d5b36a56674a55982
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-05-09 15:59:50 +0530

    Create resource.tf

---
+++ count_argument/resource.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "eoc-instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  associate_public_ip_address = true
+  subnet_id = aws_subnet.dev-subnet-public-1.id
+  security_groups = [aws_security_group.ssh-allowed.id]
+  tags = {
+      Name = var.instance-name
+   }
+}
