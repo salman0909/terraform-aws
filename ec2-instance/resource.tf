f4a8950f185ea08f26165ad6bc8660ff737a8d7c
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-05-09 10:38:02 +0530

    Update resource.tf

--- ec2-instance/resource.tf
+++ ec2-instance/resource.tf
@@ -2,7 +2,8 @@ resource "aws_instance" "eoc-instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
   associate_public_ip_address = true
-  key_name = aws_key_pair.key_pair.key_name
+  key_name = aws_key_pair.key_pair.id
+  subnet_id = aws_subnet.dev-subnet-public-1.id
   security_groups = [aws_security_group.ssh-allowed.id]
   tags = {
       Name = "eoc-instance"
