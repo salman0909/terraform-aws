f048efa544e0a3c96d6c63fe485d96b9716a5bd9
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-05-09 10:38:57 +0530

    Update resource.tf

--- ec2-instance/resource.tf
+++ ec2-instance/resource.tf
@@ -1,15 +1,3 @@
-resource "aws_instance" "eoc-instance" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  associate_public_ip_address = true
-  key_name = aws_key_pair.key_pair.id
-  subnet_id = aws_subnet.dev-subnet-public-1.id
-  security_groups = [aws_security_group.ssh-allowed.id]
-  tags = {
-      Name = "eoc-instance"
-   }
-}
-
 #create a Pem file formatted private key
 resource "tls_private_key" "rsa-4096" {
   algorithm = "RSA"
@@ -69,5 +57,16 @@ resource "aws_security_group" "ssh-allowed" {
     tags = {
         Name = "ssh-allowed"
     }
+}
+resource "aws_instance" "eoc-instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  associate_public_ip_address = true
+  key_name = aws_key_pair.key_pair.id
+  subnet_id = aws_subnet.dev-subnet-public-1.id
+  security_groups = [aws_security_group.ssh-allowed.id]
+  tags = {
+      Name = "eoc-instance"
+   }
 }
 
