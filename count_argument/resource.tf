57e8512a4970593a9dd0472e3c32b9795dc83353
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-05-09 16:23:52 +0530

    Update resource.tf

--- count_argument/resource.tf
+++ count_argument/resource.tf
@@ -2,9 +2,8 @@ resource "aws_instance" "eoc-instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
   vpc_security_group_ids = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id ]
-  #instance_type = var.instance_type_list[1]  # For List
-  count = 3
+  count = lenght(var.instance_name)
   tags = {
-      "Name" = "var.instance_name-${count.index}"
+      "Name" = "var.instance_name[count.index]"
    }
 }
