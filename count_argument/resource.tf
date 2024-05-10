bad502d6c56cea2b30cda39c3017927090f930fd
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-05-10 10:48:48 +0530

    Update resource.tf

--- count_argument/resource.tf
+++ count_argument/resource.tf
@@ -6,19 +6,19 @@ resource "aws_vpc" "main-vpc" {
   }
 }
 #create a public Subnet
-resource "aws_subnet" "dev-subnet-public-1" {
+resource "aws_subnet" "demo-subnet" {
   vpc_id = "${aws_vpc.main-vpc.id}"
   cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
   availability_zone = "us-east-2a"
   tags = {
-     Name = "dev-subnet-public-1"
+     Name = "demo-subnet"
   }
 }
 resource "aws_instance" "eoc-instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id ]
+  security_groups = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id ]
   count = lenght(var.instance_name)
   tags = {
       "Name" = "var.instance_name[count.index]"
