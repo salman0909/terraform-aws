2ba97fdbe9f8f8b30afd4f6f34dd206b8ca3a692
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-05-10 11:28:14 +0530

    Update resource.tf

--- count_argument/resource.tf
+++ count_argument/resource.tf
@@ -18,9 +18,10 @@ resource "aws_subnet" "demo-subnet" {
 resource "aws_instance" "eoc-instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  subnet_id = aws_subnet.demo-subnet.id
   security_groups = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id ]
   count = length(var.instance_name)
   tags = {
-      "Name" = "var.instance_name[count.index]"
+      Name = var.instance_name[count.index]
    }
 }
