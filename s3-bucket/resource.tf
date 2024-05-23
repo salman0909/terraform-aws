99e182aa9145d4ba65e57476ee625449506facba
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-05-23 14:09:38 +0530

    Update resource.tf

--- s3-bucket/resource.tf
+++ s3-bucket/resource.tf
@@ -1,3 +1,21 @@
+#create a VPC
+resource "aws_vpc" "main-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main-vpc"
+  }
+}
+#create a public Subnet
+resource "aws_subnet" "subnet-public" {
+  vpc_id = "${aws_vpc.main-vpc.id}"
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-2a"
+  tags = {
+     Name = "subnet-public"
+  }
+}
+
 #create s3 bucket
 resource "aws_s3_bucket" "mybucket" {
   bucket = var.bucketname
