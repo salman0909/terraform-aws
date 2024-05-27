539b53a601aa2d5b89ea3577489c6cfb4050ee5d
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-05-27 16:18:39 +0530

    Update variable.tf

--- rds_instance/variable.tf
+++ rds_instance/variable.tf
@@ -11,12 +11,34 @@ variable "aws_region" {
   default = "us-east-2" //chnage to your desired region
 }
 
+variable "engine" {
+  default     = "mysql"
+  description = "Engine type, here it is mysql"
+}
+variable "engine_version" {
+  description = "Engine version"
+
+  default = {
+    mysql    = "8.0.31"
+  }
+}
+
+variable "identifier" {
+  default     = "eocdb-rds"
+  description = "Identifier for your DB"
+}
+variable "username" {
+  default     = "eoc-admin"
+  description = "User name"
+}
+variable "db_password" {
+  description = "password, provide through your ENV variables"
+  sensitive   = true
+}
+
 variable "instance_class" {
   description = "Instance class for RDS Instance"
   type = string
   default = "db.t2.micro"
 }
-variable "db_password" {
-  description = "RDS root user password"
-  sensitive   = true
-}
+
