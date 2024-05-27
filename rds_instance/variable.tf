f1bbb21b0191ca675a77ba0ecd88a3ecdc3fb571
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-05-27 15:17:26 +0530

    Create variable.tf

---
+++ rds_instance/variable.tf
@@ -0,0 +1,18 @@
+variable "access_key" {
+
+}
+variable "secret_access_key" {
+ 
+}
+
+variable "aws_region" {
+  type = string
+  description = "AWS Region"
+  default = "us-east-2" //chnage to your desired region
+}
+
+variable "instance_class" {
+  description = "Instance class for RDS Instance"
+  type = string
+  default = "db.t2.micro"
+}
