e25f4596e2e3d8205060620074fb76ddf055937b
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-05-28 16:11:36 +0530

    Update main.tf

--- remote-s3-backend/dynamoDB/main.tf
+++ remote-s3-backend/dynamoDB/main.tf
@@ -1,15 +1,3 @@
-erraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-2"
-}
-
 resource "aws_dynamodb_table" "state_lock_table" {
   name           = "terraform_state_lock"
   billing_mode   = "PAY_PER_REQUEST"
