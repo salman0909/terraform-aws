89b64b045719938afec1f3c01677f3fd8b8f0e43
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-05-28 10:42:34 +0530

    Update output.tf

--- rds_instance/output.tf
+++ rds_instance/output.tf
@@ -1,17 +1,14 @@
 output "rds_hostname" {
   description = "RDS instance hostname"
   value       = aws_db_instance.eoc_db.address
-  sensitive   = true
 }
 
 output "rds_port" {
   description = "RDS instance port"
   value       = aws_db_instance.eoc_db.port
-  sensitive   = true
 }
 
 output "rds_username" {
   description = "RDS instance root username"
   value       = aws_db_instance.eoc_db.username
-  sensitive   = true
 }
