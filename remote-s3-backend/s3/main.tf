14c6478945e45c617cb331de49c64a5066a56c3b
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-05-28 16:08:20 +0530

    Update main.tf

--- remote-s3-backend/s3/main.tf
+++ remote-s3-backend/s3/main.tf
@@ -1,1 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
 
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "terraform-state" {
+  bucket = "eoc_bucketd"
+}
+
+resource "aws_s3_bucket_versioning" "terraform-state" {
+  bucket = aws_s3_bucket.eoc_bucketd.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
