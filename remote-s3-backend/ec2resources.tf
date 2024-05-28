668b7fe4944567ad006511aa10e116ebb99b879c
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-05-28 15:33:27 +0530

    Update ec2resources.tf

--- remote-s3-backend/ec2resources.tf
+++ remote-s3-backend/ec2resources.tf
@@ -21,6 +21,6 @@ resource "aws_default_subnet" "subnet_az2" {
 }
 
 resource "aws_instance" "terraform-state-test" {
-  ami           = "ami-0cf2b4e024cdb6960"
+  ami           = "var."
   instance_type = "t2.micro"
 }
