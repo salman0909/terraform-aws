65a9c14e491932b9c9bb2e00e19fea7417e6e652
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-05-28 16:04:40 +0530

    Create main.tf

---
+++ remote-s3-backend/s3/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_dynamodb_table" "state_lock_table" {
+  name           = "terraform_state_lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
