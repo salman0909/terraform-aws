91cfb76af41fc8b3eff37eba09cfa4a884eb3bf2
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-05-28 15:28:26 +0530

    Update ec2resources.tf

--- remote-s3-backend/ec2resources.tf
+++ remote-s3-backend/ec2resources.tf
@@ -1,3 +1,25 @@
+#create default vpc if one does not exists
+resource "aws_default_vpc" "default_vpc" {
+  tags = {
+    Name = "default_VPC"
+  }
+}
+
+#Use data source to get all availability zones in region
+data "aws_availability_zones" "available_zones" {
+
+}
+
+#Create a default subnet in the first availabilty zone if doesnt exists
+resource "aws_default_subnet" "subnet_az1" {
+  availability_zone = data.aws_availability_zones.available_zones.names[0]
+}
+
+#Create a default subnet in the second availability zone if doesnt exists
+resource "aws_default_subnet" "subnet_az2" {
+  availability_zone = data.aws_availability_zones.available_zones.names[1]
+}
+
 resource "aws_instance" "terraform-state-test" {
   ami           = "ami-0cf2b4e024cdb6960"
   instance_type = "t2.micro"
