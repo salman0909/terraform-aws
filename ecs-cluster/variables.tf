4e8e34b1bb73e3693a279f009e74897f6d2a7360
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-03 14:17:34 +0530

    Update variables.tf

--- ecs-cluster/variables.tf
+++ ecs-cluster/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "Configuring AWS as provider"
   type        = string
-  default     = "us-east-1"
+  default     = "us-east-2"
 }
 
 # keys to the castle variable
