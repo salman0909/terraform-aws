796b0417fab515e7f6cfd6c7b41e5923231782b4
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-03 14:55:55 +0530

    Update provider.tf

--- remote-state-backend/provider.tf
+++ remote-state-backend/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "eocbucketstorebackend2024"
+    dynamodb_table = "state-lock"
+    key  = "eoc/mystatefile/terraform.tfstate"
+    region = "us-east-1"
+}
+}
+
 provider "aws" {
   region     = "us-east-2"
   access_key = var.access_key
