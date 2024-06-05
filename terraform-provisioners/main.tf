58de51405b889c61c8cceae0f6fbeee1851536ab
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-05 12:38:01 +0530

    Update main.tf

--- terraform-provisioners/main.tf
+++ terraform-provisioners/main.tf
@@ -17,4 +17,85 @@ resource "aws_subnet" "dev-public-subnet" {
   cidr_block              = "10.0.0.0/24"
   availability_zone       = "us-east-2a"
   map_public_ip_on_launch = true
+ tags = {
+   Name = "public-subnet"
 }
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main-vpc.id
+  tags ={
+    Name = "public-igw"
+}
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.main-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.dev-public-subnet.id
+  route_table_id = aws_route_table.public-rt.id
+}
+resource "aws_security_group" "webSg" {
+  name   = "web"
+  vpc_id = aws_vpc.main-vpc.id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web-sg"
+  }
+}
+resource "aws_instance" "server" {
+  ami                    = "var.ami_id"
+  instance_type          = var.instance_type
+  key_name      = aws_key_pair.example.key_name
+  vpc_security_group_ids = [aws_security_group.webSg.id]
+  subnet_id              = aws_subnet.dev-public-subnet.id
+
+  connection {
+    type        = "ssh"
+    user        = "eoc-admin"  # Replace with the appropriate username for your EC2 instance
+    private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
+    host        = self.public_ip
+  }
+
+  # File provisioner to copy a file from local to the remote EC2 instance
+  provisioner "file" {
+    source      = "app.py"  # Replace with the path to your local file
+    destination = "/home/ubuntu/app.py"  # Replace with the path on the remote instance
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Hello from the remote instance'",
+      "sudo apt update -y",  # Update package lists (for ubuntu)
+      "sudo apt-get install -y python3-pip",  # Example package installation
+      "cd /home/ubuntu",
+      "sudo pip3 install flask",
+      "sudo python3 app.py &",
+    ]
+  }
+}
+
