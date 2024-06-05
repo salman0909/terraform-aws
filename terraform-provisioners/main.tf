710ff832e7a4cba98dc6c831fb8ae1208f5f163a
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-05 15:21:02 +0530

    Update main.tf

--- terraform-provisioners/main.tf
+++ terraform-provisioners/main.tf
@@ -1,6 +1,31 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+resource "tls_private_key" "rsa-4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 resource "aws_key_pair" "example" {
-  key_name   = "terraform-demo-abhi"  # Replace with your desired key name
-  public_key = file("~/.ssh/id_rsa.pub")  # Replace with the path to your public key file
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa-4096.public_key_openssh
+}
+
+#create a local file
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa-4096.private_key_pem
+  filename =  var.key_name
 }
 
 #create a VPC
@@ -17,13 +42,13 @@ resource "aws_subnet" "dev-public-subnet" {
   cidr_block              = "10.0.0.0/24"
   availability_zone       = "us-east-2a"
   map_public_ip_on_launch = true
- tags = {
-   Name = "public-subnet"
-}
+  tags = {
+    Name = "public-subnet"
+  }
 }
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main-vpc.id
-  tags ={
+  tags = {
     Name = "public-igw"
   }
 }
@@ -70,34 +95,35 @@ resource "aws_security_group" "webSg" {
   }
 }
 resource "aws_instance" "server" {
-  ami                    = "var.ami_id"
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
-  key_name      = aws_key_pair.example.key_name
+  key_name               = aws_key_pair.example.key_name
   vpc_security_group_ids = [aws_security_group.webSg.id]
   subnet_id              = aws_subnet.dev-public-subnet.id
-
+  tags = {
+    Name = "eoc-instance"
+  }
   connection {
     type        = "ssh"
-    user        = "eoc-admin"  # Replace with the appropriate username for your EC2 instance
-    private_key = file("~/.ssh/id_rsa")  # Replace with the path to your private key
+    user        = "ubuntu"                              
+    private_key = tls_private_key.rsa-4096.private_key_pem 
     host        = self.public_ip
   }
 
   # File provisioner to copy a file from local to the remote EC2 instance
   provisioner "file" {
-    source      = "app.py"  # Replace with the path to your local file
-    destination = "/home/ubuntu/app.py"  # Replace with the path on the remote instance
+    source      = "app.py"              # Replace with the path to your local file
+    destination = "/home/ubuntu/app.py" # Replace with the path on the remote instance
   }
 
   provisioner "remote-exec" {
     inline = [
       "echo 'Hello from the remote instance'",
-      "sudo apt update -y",  # Update package lists (for ubuntu)
-      "sudo apt-get install -y python3-pip",  # Example package installation
+      "sudo apt update -y",                  # Update package lists (for ubuntu)
+      "sudo apt-get install -y python3-pip", # Example package installation
       "cd /home/ubuntu",
       "sudo pip3 install flask",
       "sudo python3 app.py &",
     ]
   }
 }
-
