37b39f47149a97b9d9fb6bac80d27feef8b00451
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-05 12:23:26 +0530

    Create variables.tf

---
+++ terraform-provisioners/variables.tf
@@ -0,0 +1,26 @@
+variable "access_key" {
+  sensitive = true
+}
+variable "secret_access_key" {
+  sensitive = true
+}
+variable "ami_id" {
+  type = string
+  description = "AMI ID for EC2 instance"
+  default = "ami-0261755bbcb8c4a84" //specify with AMI ID
+}
+
+variable "aws_region" {
+  type = string
+  description = "AWS Region"
+  default = "us-east-2" //chnage to your desired region
+}
+
+variable "instance_type" {
+  description = "Instance type for EC2 Instance"
+  type = string
+  default = "t2.micro"
+}
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
