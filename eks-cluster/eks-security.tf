2806eb00370d79efabbca405395245b2e48b698f
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-06 13:03:23 +0530

    Create eks-security.tf

---
+++ eks-cluster/eks-security.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "worker_mgmt" {
+  name_prefix = "all_worker_management"
+  vpc_id = aws_vpc.main.id
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sg-allow-tls"
+  }
+}
