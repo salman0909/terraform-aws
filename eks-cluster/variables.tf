527f343ae2018b80d2435fdb54433d12e0c60bfd
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-06 12:56:01 +0530

    Create variables.tf

---
+++ eks-cluster/variables.tf
@@ -0,0 +1,25 @@
+variable "access_key" {
+  sensitive = true
+}
+variable "secret_access_key" {
+  sensitive = true
+}
+variable "kubernetes_version" {
+  default     = 1.28
+  description = "kubernetes version"
+}
+
+variable "vpc_cidr" {
+  default     = "10.0.0.0/16"
+  description = "default CIDR range of the VPC"
+}
+variable "aws_region" {
+  default = "us-east-2"
+  description = "aws region"
+}
+variable "var_name" {
+  default = "eks-vpc"
+}
+variable "key_name" {
+  default = "eks-key"
+}
