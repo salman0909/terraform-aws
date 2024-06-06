2d9c01523382d6abbbcd47e810679d340126d270
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-06 13:04:33 +0530

    Create eks-cluster-config.tf

---
+++ eks-cluster/eks-cluster-config.tf
@@ -0,0 +1,17 @@
+resource "aws_eks_cluster" "eks" {
+  name     = "eks-demo-cluster"
+  version = var.kubernetes_version
+  role_arn = aws_iam_role.master.arn
+
+
+  vpc_config {
+    subnet_ids = [aws_subnet.public-1.id, aws_subnet.public-2.id]
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.AmazonEKSServicePolicy,
+    aws_iam_role_policy_attachment.AmazonEKSVPCResourceController,
+    aws_iam_role_policy_attachment.AmazonEKSVPCResourceController,
+  ]
+}
