876a9748620c685600686a0348c380a6b4312593
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-06 14:40:42 +0530

    Update eks-nodegroup-config.tf

--- eks-cluster/eks-nodegroup-config.tf
+++ eks-cluster/eks-nodegroup-config.tf
@@ -1,28 +1,28 @@
 resource "aws_eks_node_group" "node-group" {
-  cluster_name = aws_eks_cluster.eks.name
+  cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "eks-node-group"
-  node_role_arn = aws_iam_role.worker.arn
-  subnet_ids = [ aws_subnet.public-1.id, aws_subnet.public-2.id ]
-  capacity_type = "ON_DEMAND"
-  disk_size = "20"
-  instance_types = [ "t2.small" ]
+  node_role_arn   = aws_iam_role.worker.arn
+  subnet_ids      = [aws_subnet.public-1.id, aws_subnet.public-2.id]
+  capacity_type   = "ON_DEMAND"
+  disk_size       = "20"
+  instance_types  = ["t2.small"]
 
   remote_access {
-    ec2_ssh_key = aws_key_pair.example.key_name
-    source_security_group_ids = [ aws_security_group.worker_mgmt.id ]
+    ec2_ssh_key               = aws_key_pair.example.key_name
+    source_security_group_ids = [aws_security_group.worker_mgmt.id]
   }
-  labels = tomap({env = "dev"})
+  labels = tomap({ env = "dev" })
   scaling_config {
-    desired_size = 2 
-    max_size = 3 
-    min_size = 1
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
   }
   update_config {
     max_unavailable = 1
   }
-  depends_on = [  
+  depends_on = [
     aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
-   ]
+  ]
 }
