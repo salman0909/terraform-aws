2923dcbb0d91475ac794aa0d4925962202dd7b4b
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-06 13:01:06 +0530

    Create vpc-subnets.tf

---
+++ eks-cluster/vpc-subnets.tf
@@ -0,0 +1,28 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "EKS-VPC"
+  }
+}
+resource "aws_subnet" "public-1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-sub-1"
+  }
+}
+
+resource "aws_subnet" "public-2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-2b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-sub-2"
+  }
+}
