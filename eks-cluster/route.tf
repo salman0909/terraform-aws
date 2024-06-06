325ed14a7333d8dc382b93b0fc54486181f238d4
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-06 17:03:14 +0530

    Update route.tf

--- eks-cluster/route.tf
+++ eks-cluster/route.tf
@@ -11,6 +11,10 @@ resource "aws_route_table" "public_route_table" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.eks-igw.id
   }
+  tags = {
+    Name = "eks-route-table"
+  }
+
 }
 resource "aws_route_table_association" "rta1" {
   subnet_id      = aws_subnet.public-1.id
@@ -19,7 +23,4 @@ resource "aws_route_table_association" "rta1" {
 resource "aws_route_table_association" "rta2" {
   subnet_id      = aws_subnet.public-2.id
   route_table_id = aws_route_table.public_route_table.id
-  tags = {
-    Name = "eks-route-table2"
-  }
 }
