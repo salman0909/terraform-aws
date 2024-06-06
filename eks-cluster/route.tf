25d9726f7c515d98cdd312e698d1eb3d98fa4373
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-06 13:02:50 +0530

    Create route.tf

---
+++ eks-cluster/route.tf
@@ -0,0 +1,25 @@
+resource "aws_internet_gateway" "eks-igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "eks-igw"
+  }
+} 
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks-igw.id
+  }
+  tags = {
+    Name = "eks-route"
+  }
+}
+resource "aws_route_table_association" "rta1" {
+  subnet_id = aws_subnet.public-1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+resource "aws_route_table_association" "rta2" {
+  subnet_id = aws_subnet.public-2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
