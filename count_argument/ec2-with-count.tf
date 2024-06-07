d27494ded3b88a23c071d715b52aa38b03b904c8
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-07 16:15:47 +0530

    Update ec2-with-count.tf

--- count_argument/ec2-with-count.tf
+++ count_argument/ec2-with-count.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "eoc-instance" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   subnet_id = aws_subnet.demo-subnet.id
-  security_groups = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id ]
+  security_groups = [ aws_security_group.vpc-ssh.id ]
   count = length(var.instance_name)
   tags = {
       Name = var.instance_name[count.index]
