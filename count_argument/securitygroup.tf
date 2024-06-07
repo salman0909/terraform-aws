742f78c6270f14ebf51b7d68ffca5eef44be3d7f
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-07 15:26:44 +0530

    Update securitygroup.tf

--- count_argument/securitygroup.tf
+++ count_argument/securitygroup.tf
@@ -23,33 +23,4 @@ resource "aws_security_group" "vpc-ssh" {
   }
 }
 
-# Create Security Group - Web Traffic
-resource "aws_security_group" "vpc-web" {
-  vpc_id = "${aws_vpc.main-vpc.id}"
-  description = "Dev VPC Web"
-  ingress {
-    description = "Allow Port 80"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    description = "Allow Port 443"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    description = "Allow all ip and ports outbound"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
-  tags = {
-    Name = "vpc-web"
-  }
-}
