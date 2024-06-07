9ff4524ff5bbe9df723e74719ed805c2342d0a0b
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-07 17:14:39 +0530

    Update and rename output.tf to outputs.tf

--- rds_instance/outputs.tf
+++ rds_instance/outputs.tf
@@ -2,12 +2,10 @@ output "rds_hostname" {
   description = "RDS instance hostname"
   value       = aws_db_instance.eoc_db.address
 }
-
 output "rds_port" {
   description = "RDS instance port"
   value       = aws_db_instance.eoc_db.port
 }
-
 output "rds_username" {
   description = "RDS instance root username"
   value       = aws_db_instance.eoc_db.username
