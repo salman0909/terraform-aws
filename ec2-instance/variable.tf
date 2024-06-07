91161ae747a469f12fa51c3bb58b5ed8bb65e452
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-07 11:11:25 +0530

    Update variable.tf

--- ec2-instance/variable.tf
+++ ec2-instance/variable.tf
@@ -4,18 +4,11 @@ variable "access_key" {
 variable "secret_access_key" {
   sensitive = true
 }
-variable "ami_id" {
-  type = string
-  description = "AMI ID for EC2 instance"
-  default = "ami-0a0277ba899dd9fd3" //specify with AMI ID
-}
-
 variable "aws_region" {
   type = string
   description = "AWS Region"
-  default = "us-east-2" //chnage to your desired region
+  default = "us-east-2" //change to your desired region
 }
-
 variable "instance_type" {
   description = "Instance type for EC2 Instance"
   type = string
