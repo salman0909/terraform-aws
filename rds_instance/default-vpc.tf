b3a815d83d561ce87c4e7192e4352dc61a9b7b18
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-07 17:00:59 +0530

    Create default-vpc.tf

---
+++ rds_instance/default-vpc.tf
@@ -0,0 +1,18 @@
+#create default vpc if one does not exists
+resource "aws_default_vpc" "default_vpc" {
+  tags = {
+    Name = "default-VPC"
+  }
+}
+#Use data source to get all availability zones in region
+data "aws_availability_zones" "available_zones" {
+
+}
+#Create a default subnet in the first availabilty zone if doesnt exists
+resource "aws_default_subnet" "subnet_az1" {
+  availability_zone = data.aws_availability_zones.available_zones.names[0]
+}
+#Create a default subnet in the second availability zone if doesnt exists
+resource "aws_default_subnet" "subnet_az2" {
+  availability_zone = data.aws_availability_zones.available_zones.names[1]
+}
