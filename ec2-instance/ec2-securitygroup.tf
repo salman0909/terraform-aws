ec314ded3203c8ed1d00ee0f898ab456a8e7dccf
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-07 11:03:29 +0530

    Create ec2-securitygroup.tf

---
+++ ec2-instance/ec2-securitygroup.tf
@@ -0,0 +1,26 @@
+#create a Security Group
+resource "aws_security_group" "ssh-allowed" {
+    vpc_id = "${aws_vpc.main-vpc.id}"
+    description = "Allow SSH inbound traffic"
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+   } 
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags = {
+        Name = "ssh-allowed"
+    }
+}
