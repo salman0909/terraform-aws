f01dbc5cfa05997667be5d3cb1937560f028a020
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-08 12:25:52 +0530

    Create variables.tf

---
+++ rds-instance/variables.tf
@@ -0,0 +1,34 @@
+variable "access_key" {
+  sensitive = true
+}
+variable "secret_access_key" {
+  sensitive = true
+}
+variable "engine" {
+  default     = "postgres"
+  description = "Engine type, here it is postgres"
+}
+variable "engine_version" {
+  description = "Engine version"
+  default     = "16.3"
+}
+variable "identifier" {
+  default     = "eocdb-rds"
+  description = "Identifier for your DB"
+}
+variable "username" {
+  default     = "eocadmin"
+  description = "User name"
+}
+variable "db_password" {
+  sensitive = true
+}
+variable "instance_class" {
+  description = "Instance class for RDS Instance"
+  default     = "db.t3.micro"
+}
+variable "db_name" {
+  description = "Name of the Database"
+  type        = string
+  default     = "applicationdb"
+}
