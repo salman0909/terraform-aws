596fff9b2a3b5e9ffd6a69035c0e752be70528b3
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-10 11:59:08 +0530

    Update variables.tf

--- s3-bucket/variables.tf
+++ s3-bucket/variables.tf
@@ -1,15 +1,14 @@
 variable "access_key" {
-
+  sensitive = true
 }
 variable "secret_access_key" {
- 
+  sensitive = true
 }
 variable "aws_region" {
   type = string
   description = "AWS Region"
   default = "us-east-2" //chnage to your desired region
 }
-
 
 variable "bucketname" {
   description = "The name of the s3_bucket"
