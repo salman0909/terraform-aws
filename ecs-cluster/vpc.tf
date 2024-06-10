9b9a368ab9c54193b3ea9e588320ef39403a3597
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-06-10 16:43:09 +0530

    Update vpc.tf

--- ecs-cluster/vpc.tf
+++ ecs-cluster/vpc.tf
@@ -27,7 +27,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.main_vpc.id
 
-  route = {
+  route  {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
